e2f74516f2fa67b32f930573ff86be1b942685c8
Jaden Lewis-Chambers <you@example.com>
2024-10-11 17:34:46 -0400

    lifted state up

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -22,11 +22,16 @@ export default function Navigation() {
               {/* this is a list item that is either pressable or unpressable*/}
               <Item children={<img src={logo2} />} />
               <Item children={<img src={uparrow} />} />
-              <Item
-                children={null}
-                pressable={true}
-                modal={<Modal links={["Home", "About", "Services", "Community", "Contact"]} />}
-              />
+              <Nav.Item className="flex h-[50px] w-[50px] items-center justify-center rounded-[1000px] bg-[#f0f0f0]">
+                <Nav.Trigger
+                  onClick={() => setOpen(!open)}
+                  className="flex flex-col items-center justify-center rounded-[100px]"
+                >
+                  <div className="mb-[4px] h-[2.5px] w-[22px] bg-black" />{" "}
+                  <div className="h-[2.5px] w-[22px] bg-black" />
+                </Nav.Trigger>
+                <Modal open={open} links={["Home", "About", "Services", "Community", "Contact"]} />
+              </Nav.Item>
             </Nav.List>
           </div>
         </div>
@@ -49,14 +54,6 @@ type ItemProps = UnconditionalItemProps & ConditionalItemProps;
 const Item = (props: ItemProps) => {
   return (
     <Nav.Item className="flex h-[50px] w-[50px] items-center justify-center rounded-[1000px] bg-[#f0f0f0]">
-      {props.pressable && (
-        <>
-          <Nav.Trigger className="flex flex-col items-center justify-center rounded-[100px]">
-            <div className="mb-[4px] h-[2.5px] w-[22px] bg-black" /> <div className="h-[2.5px] w-[22px] bg-black" />
-          </Nav.Trigger>
-          {props.modal}
-        </>
-      )}
       {props.children}
     </Nav.Item>
   );
@@ -69,33 +66,35 @@ type ModalProps = {
 const Modal = (props: ModalProps) => {
   return (
     <>
-      <Nav.Content
-        forceMount
-        className="z-50 mb-[.8rem] rounded-[14px] border border-[#d9d9d9] bg-[#f9f9f9bf] p-[2.4rem]"
-      >
-        <div className="flex flex-wrap gap-[6px]">
-          {props.links.map((item: string) => (
-            <Nav.Item
-              key={item}
-              tabIndex={0}
-              className="gap-6px flex max-w-[100%] items-center rounded-[1000px] bg-[#f0f0f0] px-[18px] py-[16px] text-[14px] hover:cursor-pointer hover:bg-[#e6e6e6]"
-            >
-              <p className="text-[1.5rem]">{item}</p>
-            </Nav.Item>
-          ))}
-        </div>
-        <div className="pt-[2rem]" />
-        <div className="flex">
-          <p className="text-[1.6rem]">Privacy</p>
-        </div>
-        <div className="pt-[1rem]" />
-        <div className="flex flex-col gap-[8px]">
-          <p className="text-[1.6rem]">Instagram</p>
-          <p className="text-[1.6rem]">LinkedIn</p>
-        </div>
-        <div className="pt-[6.4rem]" />
-        <img src={biglogo} />
-      </Nav.Content>
+      {props.open && (
+        <Nav.Content
+          forceMount
+          className="z-50 mb-[.8rem] rounded-[14px] border border-[#d9d9d9] bg-[#f9f9f9bf] p-[2.4rem]"
+        >
+          <div className="flex flex-wrap gap-[6px]">
+            {props.links.map((item: string) => (
+              <Nav.Item
+                key={item}
+                tabIndex={0}
+                className="gap-6px flex max-w-[100%] items-center rounded-[1000px] bg-[#f0f0f0] px-[18px] py-[16px] text-[14px] hover:cursor-pointer hover:bg-[#e6e6e6]"
+              >
+                <p className="text-[1.5rem]">{item}</p>
+              </Nav.Item>
+            ))}
+          </div>
+          <div className="pt-[2rem]" />
+          <div className="flex">
+            <p className="text-[1.6rem]">Privacy</p>
+          </div>
+          <div className="pt-[1rem]" />
+          <div className="flex flex-col gap-[8px]">
+            <p className="text-[1.6rem]">Instagram</p>
+            <p className="text-[1.6rem]">LinkedIn</p>
+          </div>
+          <div className="pt-[6.4rem]" />
+          <img src={biglogo} />
+        </Nav.Content>
+      )}
     </>
   );
 };
