473922e66083596f11b16b17b6c88d0ae733bb61
Jaden Lewis-Chambers <you@example.com>
2024-10-12 01:03:36 -0400

    added mobile nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,29 +1,53 @@
 import logo from "../assets/logo.svg";
 import logo2 from "../assets/logo2.svg";
 import uparrow from "../assets/uparrow.svg";
-import { motion, AnimatePresence, useScroll, useMotionValueEvent, useTransform } from "framer-motion";
-import React, { useEffect } from "react";
+import biglogo from "../assets/biglogo.svg";
+import * as Nav from "@radix-ui/react-navigation-menu";
+import React from "react";
 
-declare module "react" {
-  interface CSSProperties {
-    [key: `--${string}`]: string | number;
-  }
-}
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Navigation() {
-  const [toggle, setToggle] = React.useState(false);
-
-  const { scrollY } = useScroll();
-
-  useMotionValueEvent(scrollY, "change", (latest) => {
-    if (latest > 80) setToggle(true);
-    else setToggle(false);
-  });
-
+  // create state variable that tracks whether the modal is open
+  const [open, setOpen] = React.useState(false);
   return (
-    <div className="bg-none">
-      <Component toggle={toggle} links={["Home", "About", "Services", "Community", "Contact"]} />
-    </div>
+    <header className="fixed z-10 flex w-full justify-between px-[4rem] py-[2rem]">
+      {open && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="pointer-events-none fixed inset-0 h-[100dvh] w-full bg-[#000000ce]"
+        />
+      )}
+
+      <Nav.Root className="fixed inset-0 top-auto py-[1.5rem]">
+        <div className="flex w-full flex-col justify-center px-[4rem]">
+          <Nav.Viewport forceMount />
+
+          <div className="flex justify-center">
+            {/* this list will be its own sub-component and it will either have links */}
+            <Nav.List className="flex gap-[1rem] rounded-[1000px] border border-[#f0f0f0] bg-[#f9f9f9bf] p-[.5rem] backdrop-blur-[10px]">
+              {/* this is a list item that is either pressable or unpressable*/}
+              <Item children={<img src={logo2} />} />
+
+              <Item children={<img src={uparrow} />} />
+
+              <Nav.Item className="flex h-[50px] w-[50px] items-center justify-center rounded-[1000px] bg-[#f0f0f0]">
+                <Nav.Trigger
+                  onClick={() => setOpen(!open)}
+                  className="flex h-full w-full flex-col items-center justify-center rounded-[100px]"
+                >
+                  <div className="mb-[4px] h-[2.5px] w-[22px] bg-black" />{" "}
+                  <div className="h-[2.5px] w-[22px] bg-black" />
+                </Nav.Trigger>
+                <Modal open={open} links={["Home", "About", "Services", "Community", "Contact"]} />
+              </Nav.Item>
+            </Nav.List>
+          </div>
+        </div>
+      </Nav.Root>
+    </header>
   );
 }
 interface CProps {
@@ -31,106 +55,68 @@ interface CProps {
   links: Array<string>;
 }
 
-const Component: React.FC<CProps> = ({ toggle, links }) => {
-  const [wait, setWait] = React.useState<boolean>(toggle);
-
-  React.useEffect(() => {
-    if (toggle == false) setWait((prev) => (prev = toggle));
-  }, [toggle]);
-
-  const v = {
-    visible: { opacity: 1 },
-    hidden: { opacity: 0 },
-  };
-
-  function func() {
-    setWait((prev) => (prev = toggle));
-    console.log(wait);
-  }
-
-  const { scrollYProgress } = useScroll();
-
-  const useYScroll = useTransform(scrollYProgress, [0, 1], [0, 500]);
-
-  const obj = {};
-
-  return (
-    <AnimatePresence initial={false}>
-      <motion.div
-        layout
-        style={{ justifyContent: toggle ? "center" : "space-between" }}
-        onLayoutAnimationComplete={func}
-        className="fixed z-[999] flex w-full items-center justify-center p-[2.5rem]"
-      >
-        <motion.img
-          variants={v}
-          animate={toggle ? "hidden" : "visible"}
-          layout
-          style={{ width: toggle ? 0 : "60px" }}
-          src={logo}
-        />
-        <motion.div
-          layout
-          className="static rounded-[1000px] border-l-[#f0f0f0] border-l-[] bg-[#f9f9f9bf] p-[.8rem] backdrop-blur-[5px]"
-          onLayoutAnimationComplete={func}
-        >
-          <div className="flex gap-[6px] overflow-clip">
-            {wait && (
-              <motion.a
-                key={1}
-                className="flex h-[50px] w-[50px] items-center justify-center rounded-[1000px] bg-[#f0f0f0] text-[1rem] leading-[1] hover:bg-[#e6e6e6]"
-              >
-                <motion.img layout src={logo2} style={{ rotate: useYScroll }} className="absolute" alt="logo" />
-              </motion.a>
-            )}
-
-            {links.map((link, index) => {
-              return (
-                <motion.a
-                  key={index}
-                  layout
-                  className="gap-6px flex max-w-[100%] items-center rounded-[1000px] bg-[#f0f0f0] px-[1.6rem] py-[1.4rem] text-[1.5rem] font-light hover:cursor-pointer hover:bg-[#e6e6e6]"
-                >
-                  {link}
-                </motion.a>
-              );
-            })}
-            {wait && (
-              <motion.a
-                key={2}
-                initial="hidden"
-                animate="visible"
-                exit="hidden"
-                className="flex h-[50px] w-[50px] items-center justify-center rounded-[1000px] bg-[#f0f0f0] text-[1rem] leading-[1] hover:bg-[#e6e6e6]"
-              >
-                <motion.img src={uparrow} className="" alt="logo" />
-              </motion.a>
-            )}
-          </div>
-        </motion.div>
-      </motion.div>
-    </AnimatePresence>
-  );
+type ConditionalItemProps = {
+  pressable?: boolean;
+  modal?: JSX.Element;
 };
 
-{
-  /* {toggle && (
-              <motion.a
-                layout
-                className="flex h-[50px] w-[50px] items-center justify-center rounded-[1000px] bg-[#f0f0f0] text-[1rem] leading-[1]"
-              >
-                <motion.img layout src={logo2} className="" alt="logo" />
-              </motion.a>
-            )} */
-}
+type ItemProps = UnconditionalItemProps & ConditionalItemProps;
 
-{
-  /* {toggle && (
-              <motion.a
-                layout
-                className="flex h-[50px] w-[50px] items-center justify-center rounded-[1000px] bg-[#f0f0f0] text-[1rem] leading-[1] hover:bg-[#e6e6e6]"
-              >
-                <img src={uparrow} className="" alt="logo" />
-              </motion.a>
-            )} */
-}
+const Item = (props: ItemProps) => {
+  return (
+    <Nav.Item className="flex h-[50px] w-[50px] items-center justify-center rounded-[1000px] bg-[#f0f0f0]">
+      {props.children}
+    </Nav.Item>
+  );
+};
+type ModalProps = {
+  links: string[];
+  open: boolean;
+};
+export const transition = {
+  type: "spring",
+  duration: 0.8,
+};
+
+const Modal = (props: ModalProps) => {
+  return (
+    <>
+      <Nav.Content forceMount asChild>
+        <AnimatePresence>
+          {props.open && (
+            <motion.div
+              className="z-50 mb-[.8rem] rounded-[14px] border border-[#d9d9d9] bg-[#f9f9f9bf] p-[2.4rem]"
+              initial={{ y: "100vh", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: "100vh", opacity: 0 }}
+              transition={transition}
+            >
+              <div className="flex flex-wrap gap-[6px]">
+                {props.links.map((item: string) => (
+                  <Nav.Item
+                    key={item}
+                    tabIndex={0}
+                    className="gap-6px flex max-w-[100%] items-center rounded-[1000px] bg-[#f0f0f0] px-[18px] py-[16px] text-[14px] hover:cursor-pointer hover:bg-[#e6e6e6]"
+                  >
+                    <p className="text-[1.5rem]">{item}</p>
+                  </Nav.Item>
+                ))}
+              </div>
+              <div className="pt-[2rem]" />
+              <div className="flex">
+                <p className="text-[1.6rem]">Privacy</p>
+              </div>
+              <div className="pt-[1rem]" />
+              <div className="flex flex-col gap-[8px]">
+                <p className="text-[1.6rem]">Instagram</p>
+                <p className="text-[1.6rem]">LinkedIn</p>
+              </div>
+              <div className="pt-[6.4rem]" />
+              <img src={biglogo} />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </Nav.Content>
+    </>
+  );
+};
